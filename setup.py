5c2897e91c1a7c9efb6af90a61d84d2318701293
williamfzc <178894043@qq.com>
2019-07-17 00:06:44 +0800

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='stagesepx',
+    version='0.1.0',
+    description='detect stages in video automatically',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/stagesepx',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'loguru',
+        'scikit-image',
+        'scikit-learn',
+        'pyecharts',
+    ]
+)
