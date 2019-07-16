1edcfd47b24d93c944d66a156536fd2594de6d43
williamfzc <178894043@qq.com>
2019-07-17 00:07:26 +0800

    test(ci): travis ci

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ from stagesepx.classifier import SSIMClassifier
 from stagesepx.reporter import Reporter
 
 
-# cut
-video_path = 'video/demo_video.mp4'
-cutter = VideoCutter(period=4)
+# # cut
+video_path = 'test.mp4'
+cutter = VideoCutter(period=2)
 res = cutter.cut(video_path)
 stable = res.get_stable_range()
 data_home = res.pick_and_save(stable, 3)
@@ -16,4 +16,4 @@ cl.load(data_home)
 res = cl.classify(video_path)
 
 # draw
-Reporter.draw(res)
+Reporter.draw(res, report_path=f'{data_home}/report.html')
