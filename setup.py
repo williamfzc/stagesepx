a3ec40e5cb37884813b5c5c0a9dc42de574f4bff
williamfzc <178894043@qq.com>
2019-08-09 23:58:07 +0800

    release: 0.4.1
    - fix #19
    - fix #24
    - video path -> video object

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stagesepx',
-    version='0.4.0',
+    version='0.4.1',
     description='detect stages in video automatically',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
