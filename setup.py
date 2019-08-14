dc5bb9d2258c79b7e0728918d5db28c740546e24
williamfzc <178894043@qq.com>
2019-08-14 15:53:32 +0800

    release: 0.4.3
    - fix #26
    - fix #27

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stagesepx',
-    version='0.4.2',
+    version='0.4.3',
     description='detect stages in video automatically',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
