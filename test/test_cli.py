697129a17b48c83720929dc5bbf2535c7333bb2b
williamfzc <178894043@qq.com>
2019-09-09 18:51:00 +0800

    fix(test): do not run cli in shell

--- test/test_cli.py
+++ test/test_cli.py
@@ -9,16 +9,16 @@ VIDEO_PATH = os.path.join(PROJECT_PATH, 'demo.mp4')
 
 def test_cli():
     logger.info('checking one_step ...')
-    subprocess.check_call(['stagesepx', 'one_step', VIDEO_PATH], shell=True)
-    subprocess.check_call(['stagesepx', 'one_step', VIDEO_PATH, 'output'], shell=True)
+    subprocess.check_call(['stagesepx', 'one_step', VIDEO_PATH])
+    subprocess.check_call(['stagesepx', 'one_step', VIDEO_PATH, 'output'])
     shutil.rmtree('output')
 
     logger.info('checking cutter ...')
-    subprocess.check_call(['stagesepx', 'cut', VIDEO_PATH], shell=True)
-    subprocess.check_call(['stagesepx', 'cut', VIDEO_PATH, 'output'], shell=True)
+    subprocess.check_call(['stagesepx', 'cut', VIDEO_PATH])
+    subprocess.check_call(['stagesepx', 'cut', VIDEO_PATH, 'output'])
 
     logger.info('checking classifier ...')
-    subprocess.check_call(['stagesepx', 'classify', VIDEO_PATH, 'output'], shell=True)
+    subprocess.check_call(['stagesepx', 'classify', VIDEO_PATH, 'output'])
     shutil.rmtree('output')
 
 # same as:
