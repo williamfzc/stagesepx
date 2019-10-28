b18d29d235da30b180de527241e49f5c6a40439c
williamfzc <178894043@qq.com>
2019-10-28 16:26:04 +0800

    chore(ci): add some cases

--- test/test_cli.py
+++ test/test_cli.py
@@ -8,6 +8,9 @@ VIDEO_PATH = os.path.join(PROJECT_PATH, "demo.mp4")
 
 
 def test_cli():
+    logger.info("checking main")
+    subprocess.check_call(["python", "-m", "stagesepx.cli"])
+
     logger.info("checking one_step ...")
     subprocess.check_call(["stagesepx", "one_step", VIDEO_PATH])
     subprocess.check_call(["stagesepx", "one_step", VIDEO_PATH, "output"])
--- test/test_toolbox.py
+++ test/test_toolbox.py
@@ -5,7 +5,10 @@ from stagesepx import toolbox
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 IMAGE_NAME = "demo.jpg"
 IMAGE_PATH = os.path.join(PROJECT_PATH, IMAGE_NAME)
+VIDEO_NAME = "demo.mp4"
+VIDEO_PATH = os.path.join(PROJECT_PATH, VIDEO_NAME)
 assert os.path.isfile(IMAGE_PATH), f"{IMAGE_NAME} not existed!"
+assert os.path.isfile(VIDEO_PATH), f"{VIDEO_PATH} not existed!"
 
 
 def test_turn_blur():
@@ -32,3 +35,24 @@ def test_turn_surf_desc():
 def test_turn_lbp_desc():
     image = toolbox.imread(IMAGE_PATH)
     toolbox.turn_lbp_desc(image)
+
+
+def test_get_frame():
+    with toolbox.video_capture(VIDEO_PATH) as cap:
+        first = 5
+        second = 8
+
+        actual = toolbox.get_frame_time(cap, first)
+        should = toolbox.get_current_frame_time(cap)
+        # should be frame 5
+        assert actual == should
+
+        # 5 -> 8 -> 5
+        frame = toolbox.get_frame(cap, second, True)
+        assert frame is not None
+        assert toolbox.get_current_frame_id(cap) == first
+
+        # 5 -> 8
+        frame = toolbox.get_frame(cap, second)
+        assert frame is not None
+        assert toolbox.get_current_frame_id(cap) == second
