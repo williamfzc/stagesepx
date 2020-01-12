cca5d1a517c77f7a1aaa7b9577ef1f198470e66b
williamfzc <178894043@qq.com>
2020-01-12 20:05:21 +0800

    chore(example): more desc

--- example/with_keras.py
+++ example/with_keras.py
@@ -1,6 +1,8 @@
 """
 classify with keras model
 """
+from keras.models import Sequential
+
 from stagesepx.cutter import VideoCutter
 from stagesepx.classifier.keras import KerasClassifier
 from stagesepx.reporter import Reporter
@@ -18,6 +20,24 @@ stable, unstable = res.get_range()
 data_home = res.pick_and_save(stable, 10)
 
 # --- classify ---
+# We recommend that you read the code (KerasClassifier) directly for better understanding
+# and actually you can build your own Classifier which based on it
+class NewKerasClassifier(KerasClassifier):
+    def create_model(self) -> Sequential:
+        # overwrite this method to design your own model structure!
+
+        # model  = Sequential()
+        # ...
+        pass
+
+    def train(self, data_path: str, *_, **__):
+        # ...
+        pass
+
+
+# or use the default one
+# and then init it
+# epochs=1 is just a example
 cl = KerasClassifier(epochs=1)
 
 # train model and save weights
