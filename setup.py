a84a64ef7c746393585825dc54632b85d139a58a
williamfzc <178894043@qq.com>
2020-02-10 19:33:46 +0800

    chore(#94): remove opencv-contrib-python

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         "opencv-python>=4.1.0.25",
-        "opencv-contrib-python==3.4.2.17",
         "numpy>=0.16.2",
         "loguru>=0.2.5",
         "scikit-image>=0.16.0",
