589fcb52eeefbf9f2d3137c86a0c6a0532b2053d
williamfzc <178894043@qq.com>
2020-03-10 20:01:32 +0800

    test(classifier): new api

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -102,6 +102,8 @@ def test_result():
     classify_result.get_stage_range()
     classify_result.get_specific_stage_range("0")
     classify_result.get_not_stable_stage_range()
+    classify_result.mark_range_ignore(23, 24)
+    classify_result.time_cost_between("0", "1")
     assert classify_result.first("1").frame_id == 20
     assert classify_result.last("1").frame_id == 21
 
