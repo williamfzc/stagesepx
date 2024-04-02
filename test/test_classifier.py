5f214ae26fbaec7e87e4bd61fc4748f8fb074c36
williamfzc <178894043@qq.com>
2024-04-02 11:19:28 +0800

    fix(ci): weight file suffix

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -130,10 +130,11 @@ def test_keras():
     # set epochs to 1 for quickly training (test only)
     cl = KerasClassifier(epochs=1)
     cl.train(CUTTER_RESULT_DIR)
-    cl.save_model("haha.h5")
+    weight_file = "haha.weights.h5"
+    cl.save_model(weight_file)
     # recreate
     cl = KerasClassifier()
-    cl.load_model("haha.h5")
+    cl.load_model(weight_file)
     stable, _ = cutter_res.get_range()
     classify_result = cl.classify(VIDEO_PATH, stable, keep_data=True)
     assert classify_result.to_dict()
